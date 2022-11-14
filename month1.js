bce24609f65322145c69bca6488e2fe45c6d3af1
Senadin Beganovic <beganovicsenadin24@gmail.com>
2022-11-14 14:42:40 +0100

    added more solved algos

--- month1.js
+++ month1.js
@@ -263,6 +263,222 @@
 // };
 
 // console.log(sumOfThree(36));
+ 
+// function senate(senate){
+//   senate = senate.split("");
+//   console.log(senate.indexOf("D"))
+//     while(senate.includes("R")&&senate.includes("D")){
+//         if(senate[0]==="R") senate.splice(senate.indexOf("D"), 1);
+//         else senate.splice(senate.indexOf("R"), 1);
+//         senate.push(senate.shift());
+//     }
+//     return senate[0]==="R" ?  "Radiant" : "Dire";
+// }
+
+// https://leetcode.com/problem-list/top-interview-questions/
+
+// const nextGreatestLetter = function(letters, target) {
+//   const indexOfFristLargest = letters.findIndex((letter) => letter > target)
+
+//   return letters[indexOfFristLargest === -1 ? 0 : indexOfFristLargest];
+// }
+
+// console.log(nextGreatestLetter(["c","f","j"], "a"))
+
+// var checkString = function (string) {
+//   let i = 0;
+//   let foundFirstB = false;
+//   while(i < string.length){
+//     if(string[i] === "b"){
+//       foundFirstB = true;
+//     }
+//     if(foundFirstB && string[i] === 'a'){
+//       return false;
+//     }
+//     i++;
+//   }
+
+//   return true;
+// };
+
+// SOLUTION 2
+// var checkString = function (string) {
+// return (
+//   s.indexOf("b") === -1 ||                          // is b is not there ?
+//   s.lastIndexOf("a") < s.indexOf("b")               // is  last a  before  1st b ?
+// );
+//}
+// const maxProduct = (nums) => {
+//   const maxNum1 = Math.max(...nums);
+//   nums.splice(nums.indexOf(maxNum1), 1);
+//   const maxNum2 = Math.max(...nums)
+//   return (maxNum1-1)*(maxNum2-1);
+
+// };
+
+// const maxProduct = nums => {
+//   let m1 = 0, m2 = 0;
+//   for (const val of nums) {
+//     m2 = Math.max(m2, Math.min(m1, val));
+//     m1 = Math.max(m1, val);
+//   }
+//   return (m1 - 1) * (m2 - 1);
+// };
+
+// const maxProduct = nums => {
+//   let max = 0;
+//   for (let i = 0, j = nums.length - 1; i < j;) {
+//     max = Math.max(max, (nums[i] - 1) * (nums[j] - 1));
+//     nums[i] < nums[j] ? ++i : --j;
+//   }
+//   return max;
+// };
+
+// const maxProduct = (
+//   nums,
+//   max = nums.reduce(
+//     (prev, val) => [
+//       Math.max(prev[1], Math.min(prev[0], val)),
+//       Math.max(prev[0], val),
+//     ],
+//     [0, 0]
+//   )
+// ) => (max[0] - 1) * (max[1] - 1);
+
+// const numOfBurgers = (tomatoSlices, cheeseSlices) => {
+//  if(tomatoSlices === 0 && cheeseSlices === 0) return [0,0]
+//  let [totalNumSmall, restOfSmallTomatoes, restOfSmallCheese] =  calculateMaxSlices(tomatoSlices, cheeseSlices, 2)
+
+//  while(totalNumSmall > 0 && (restOfSmallTomatoes > 0 || restOfSmallCheese > 0)){
+//    totalNumSmall--;
+//    restOfSmallTomatoes = restOfSmallTomatoes + 2;
+//    restOfSmallCheese++;
+//    let [totalNumJumbo, restOfJumboTomatoes, restOfJumboCheese] =  calculateMaxSlices(restOfSmallTomatoes, restOfSmallCheese, 4)
+
+//    if(restOfJumboTomatoes === 0 && restOfJumboCheese === 0) {
+//     return [totalNumJumbo , totalNumSmall]
+//    }
+//  }
+//  if(totalNumSmall === 0) return []
+//  return [0, totalNumSmall]
+// };
+
+// const calculateMaxSlices = (tomatoSlices, cheeseSlices, divisionNumTomatoes) => {
+//   let total = 0;
+//   while(tomatoSlices > 0 && cheeseSlices > 0){
+//     tomatoSlices = tomatoSlices - divisionNumTomatoes;
+//     cheeseSlices = cheeseSlices -1;
+//     total++;
+//   }
+//   return [total, tomatoSlices, cheeseSlices];
+// }
+
+// var numOfBurgers = function(tomatoSlices, cheeseSlices) {
+//   let jumbo = (tomatoSlices - 2 * cheeseSlices) / 2;
+//   let small = cheeseSlices - jumbo;
+//   return jumbo >= 0 && small >= 0 && jumbo % 1 == 0 ? [jumbo, small] : [];
+// };
+
+// console.log(numOfBurgers(8,2))
+
+// var maxOperations = function(nums, k) {
+//   nums.sort((a,b) => a-b);
+//   let count = 0;
+//   let left = 0, right = nums.length - 1;
+
+//   while(left < right){
+//     const tempSum = nums[left] + nums[right];
+//     if(tempSum == k){
+//       count++;
+//       left++;
+//       right--;
+//     } else if ( tempSum < k){
+//       left++;
+//     } else {
+//       right--;
+//     }
+//   }
+
+//   return count;
+// }
+
+// var maxOperations = function (nums, k) {
+//   const hashMap = new Map();
+//   let operationsCount = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const diff = k - nums[i];
+
+//     // if the current difference is not found then set the value to map
+//     if (!hashMap.has(diff)) {
+
+//       hashMap.set(nums[i], (hashMap.get(nums[i]) || 0) + 1);
+//       continue;
+//     }
+//     // In previous condition we checked that the hash presents
+//     // reduce the occurrance by 1
+//     hashMap.set(diff, hashMap.get(diff) - 1);
+//     // if the value is 0 of hash then delete 
+
+//     // 0 means the hash has no more occurrances
+//     if (!hashMap.get(diff)) hashMap.delete(diff);
+//     // increase the operation count
+//     operationsCount++;
+//   }
+//   return operationsCount;
+// };
+
+// console.log(
+//   maxOperations([1,2,4,5], 6)
+// );
+
+
+// var generateParenthesis = function(n){
+//   const combos = [];
+
+//   function helper(combo, lefts, total){
+//     if(total === n * 2){
+//       if(lefts === 0){
+//         combos.push(combo)
+//       }
+//       return;
+//     }
+//     if(lefts < 0) return;
+//     if(lefts > n) return;
+//     helper(combo + "(", lefts + 1, total + 1)
+//     helper(combo + ")", lefts - 1, total + 1)
+//   }
+      
+//   helper("(", 1, 1)
+
+//   return combos;
+// }
+
+// console.log(
+//   generateParenthesis(3)
+// );
+
+
+// var rob = function(nums) {
+//   return Math.max(helper(nums.slice(0, nums.length - 1)), helper(nums.slice(1, nums.length)), nums[0])
+  
+//   function helper(nums){
+//       console.log(nums)
+//       let rob1 = 0;
+//       let rob2 = 0;
+      
+//       for (const n of nums){
+//           let newRob = Math.max(rob1 + n, rob2);
+//           rob1 = rob2;
+//           rob2 = newRob; 
+//       }
+//       return rob2;
+      
+//   }
+// };
+// console.log(rob([200,3,140,20,10]))
+
+// console.log(rob([1,7,1,1,1,7]))
 
 // https://leetcode.com/problems/find-median-from-data-stream/
 
